3f84a58180334f42c251212163a9396fa82c4331
Kirubakaran <karankrish3497@gmail.com>
2026-08-11 20:35:16 +0530

    Generate sitemap from calculator registry

--- scripts/generate-sitemap.ts
+++ scripts/generate-sitemap.ts
@@ -17,17 +17,12 @@ const routes = [
     ...calculatorRoutes,
 ];
 
-const sitemap = `
-    <?xml version="1.0" encoding="UTF-8"?> 
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-        ${routes
-            .map(
-                (route) => `<url> <loc>${BASE_URL}${route}</loc> </url>`
-            )
-            .join("\n")
-        } 
-    </urlset>
-`;
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?> <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${routes
+    .map(
+        (route) => `    <url> <loc>${BASE_URL}${route}</loc> </url>`
+    )
+    .join("\n")} </urlset>`;
 
 const outputPath = path.resolve("public/sitemap.xml");
 
